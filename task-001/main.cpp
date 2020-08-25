7e26408b3a22f5cca5596be685d2c2b1e5b3b5ae
suzuki-navi <suzuki.navi.dev@gmail.com>
2020-08-25 22:12:19 +0900

    add bin/execute and support cpp

---
+++ task-001/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  string t1, t2;
+  cin >> t1 >> t2;
+  string s2;
+  cin >> s2; // これでは1行入力にはならない
+  cout << (a + b + c) << endl;
+  cout << t1 << t2 << endl;
+  cout << s2 << endl;
+}
